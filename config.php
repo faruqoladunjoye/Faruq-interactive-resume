4f63f469716776892fef31f8cdb8b9c6f55a9a3f
Faruq Oladunjoye <49567943+Faruq000@users.noreply.github.com>
2021-08-20 08:02:52 -0400

    New database

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_dbname = "resume";
+// $mysql_hostname = "localhost";
+// $mysql_user = "root";
+// $mysql_password = "";
+// $mysql_dbname = "resume";
+
+$mysql_hostname = "remotemysql.com";
+$mysql_user = "XQWlof3Dot";
+$mysql_password = "ENTbjsG6S6";
+$mysql_dbname = "XQWlof3Dot";
 
 
 $conn = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_dbname);
